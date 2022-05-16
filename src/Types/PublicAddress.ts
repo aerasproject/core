0b867d87e166311cdcc6186a30d96197460f0135
rbpolim <rodrigobpolim@gmail.com>
2022-05-16 11:46:53 -0300

    enhacement: atualizado publicAddress

--- src/Types/PublicAddress.ts
+++ src/Types/PublicAddress.ts
@@ -1,12 +1,12 @@
 export interface IPublicAddress {
-    street: string;
-    number: string;
-    city: string;
-    state: string;
-    neighborhood: string;
-    postalCode: string;
-    complement: string;
-    technicianId: string;
-    createdAt: string;
+  street: string; 
+  number: string;
+  city: string;
+  state: string;
+  neighborhood: string;
+  postalCode: string;
+  complement: string;
+  ownerId: string;
+  createdAt: string;
 	updatedAt: string;
 }
