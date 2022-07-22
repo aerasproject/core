99ae3f7081f0468158402d6f1e1fddb1a7037999
rbpolim <rodrigobpolim@gmail.com>
2022-07-22 15:58:03 -0300

    feat: some improves

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,46 @@
-import axios from 'axios';
+import axios from "axios";
 
 // SERVICES
-export * from './Services/Configs';
-export * from './Services/Api';
+export * from "Services/Configs";
+export * from "Services/Api";
 
 // CONTROLLERS BETA
-import { BetaCompanyController } from './Controllers/beta.company.controller';
-import { BetaAddressController } from './Controllers/beta.address.controller';
-import { BetaEnvironmentController } from './Controllers/beta.environment.controller';
-import { BetaEquipmentController } from './Controllers/beta.equipment.controller';
-import { BetaPublicAddressController } from './Controllers/beta.publicAddress.controller';
-import { BetaTechnicianController } from './Controllers/beta.technician.controller';
-import { BetaUserController } from './Controllers/beta.user.controller';
+import { BetaCompanyController } from "Controllers/beta.company.controller";
+import { BetaAddressController } from "Controllers/beta.address.controller";
+import { BetaEnvironmentController } from "Controllers/beta.environment.controller";
+import { BetaEquipmentController } from "Controllers/beta.equipment.controller";
+import { BetaPublicAddressController } from "Controllers/beta.publicAddress.controller";
+import { BetaTechnicianController } from "Controllers/beta.technician.controller";
+import { BetaUserController } from "Controllers/beta.user.controller";
 
 // CONTROLLERS
-export * from './Controllers/user.controller';
-export * from './Controllers/beta.user.controller';
-export * from './Controllers/address.controller';
-export * from './Controllers/environment.controller';
-export * from './Controllers/equipment.controller';
-export * from './Controllers/publicAddress.controller';
-export * from './Controllers/technician.controller';
-export * from './Controllers/company.controller';
+export * from "Controllers/user.controller";
+export * from "Controllers/beta.user.controller";
+export * from "Controllers/address.controller";
+export * from "Controllers/environment.controller";
+export * from "Controllers/equipment.controller";
+export * from "Controllers/publicAddress.controller";
+export * from "Controllers/technician.controller";
+export * from "Controllers/company.controller";
+export * from "Controllers/authentication.controller";
 
 // TYPE
-export * from './Types/User';
-export * from './Types/Address';
-export * from './Types/Environment';
-export * from './Types/Equipment';
-export * from './Types/Technician';
-export * from './Types/PublicAddress';
-export * from './Types/Company';
+export * from "Types/User";
+export * from "Types/Address";
+export * from "Types/Environment";
+export * from "Types/Equipment";
+export * from "Types/Technician";
+export * from "Types/PublicAddress";
+export * from "Types/Company";
+export * from "Types/Authentication";
 
 // UTILS
-export * from './Utils/ApiErrorInterceptor';
+export * from "Utils/ApiErrorInterceptor";
 
 // ENUMS
-export * from './Enums/UserRoles';
-export * from './Enums/TechnicianServices';
-export * from './Enums/CompanyServices';
+export * from "Enums/UserRoles";
+export * from "Enums/TechnicianServices";
+export * from "Enums/CompanyServices";
 
 export interface InitCoreParams {
   apiUrl: string;
@@ -58,7 +60,7 @@ export function initCore({ apiUrl }: InitCoreParams) {
   const TechnicianController = new BetaTechnicianController(Api);
 
   return {
-    Api, 
+    Api,
     UserController,
     CompanyController,
     AddressController,
