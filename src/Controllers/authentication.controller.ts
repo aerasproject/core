d8b28bb3f61f48083090ca2f38b1b1394682c542
rbpolim <rodrigobpolim@gmail.com>
2022-07-22 15:58:27 -0300

    feat: new Authentication structure

---
+++ src/Controllers/authentication.controller.ts
@@ -0,0 +1,11 @@
+import { Api } from "Services";
+
+import { ILoginParams, ITokenResponse } from "Types/Authentication";
+
+export class Authentication {
+  public async login(data: ILoginParams): Promise<ITokenResponse> {
+    const response = await Api.post("/authenticate", data);
+
+    return response.data;
+  }
+}
