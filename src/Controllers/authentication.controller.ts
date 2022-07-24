8b3edbe5d65b9e1567d93c5905190e9bfd076e7a
rbpolim <rodrigobpolim@gmail.com>
2022-07-23 22:49:55 -0300

    feat: updated authentication controller

--- src/Controllers/authentication.controller.ts
+++ src/Controllers/authentication.controller.ts
@@ -2,7 +2,7 @@ import { Api } from "../Services";
 
 import { ILoginParams, ITokenResponse } from "../Types/Authentication";
 
-export class Authentication {
+export class AuthenticationController {
   public async login(data: ILoginParams): Promise<ITokenResponse> {
     const response = await Api.post("/authenticate", data);
 
