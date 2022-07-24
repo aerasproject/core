217e3afa016ac6d37d21c08f91323ef9eb66cbea
rbpolim <rodrigobpolim@gmail.com>
2022-07-23 22:57:22 -0300

    feat: imported equipment controller and beta equip controller

--- src/Controllers/index.ts
+++ src/Controllers/index.ts
@@ -2,12 +2,14 @@ export * from "./user.controller";
 export * from "./address.controller";
 export * from "./environment.controller";
 export * from "./publicAddress.controller";
+export * from "./equipment.controller";
 export * from "./technician.controller";
 export * from "./company.controller";
 export * from "./authentication.controller";
 
 export * from "./beta.user.controller";
 export * from "./beta.address.controller";
+export * from "./beta.equipment.controller";
 export * from "./beta.environment.controller";
 export * from "./beta.publicAddress.controller";
 export * from "./beta.technician.controller";
